78d8836b9d0eca060bf798eee44fdeece69572e4
yunik1004 <inkyuhak@gmail.com>
2019-11-28 20:18:11 +0900

    Initial commit - implement basic parser

---
+++ collect.py
@@ -0,0 +1,33 @@
+import sys
+import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+import networkx as nx
+import requests
+
+
+if __name__ == "__main__":
+    graph = nx.DiGraph()
+
+    url = "http://openapi.its.go.kr/dev/TrafficInfo.xml;jsessionid=533FDD030B9595A85D82E02634DEB80F" # Should be changed
+    response = requests.get(url)
+    print("Status code : ", response.status_code)
+
+    xmlData = response.text
+    root = ET.fromstring(xmlData)
+
+    for data in root.findall('data'):
+        startnodeid = int(data.find('startnodeid').text)
+        endnodeid = int(data.find('endnodeid').text)
+        avgspeed = float(data.find('avgspeed').text)
+        traveltime = int(data.find('traveltime').text)
+
+        graph.add_edge(startnodeid, endnodeid, avgspeed=avgspeed, traveltime=traveltime)
+    #endfor
+
+    fig = plt.figure()
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos, node_size=30)
+    nx.draw_networkx_edges(graph, pos)
+
+    plt.show()
+#endif
