d140d7db3566e55cf4cf8356261d69aa63183e04
yunik1004 <inkyuhak@gmail.com>
2019-11-29 14:04:35 +0900

    Clean the dataset

--- collectNodeData.py
+++ collectNodeData.py
@@ -14,23 +14,35 @@ def download (url, filepath):
             response = requests.get(url)
             file.write(response.content)
         #endwith
+    #endif
+#enddef
+
+
+# Extract the file
+def extract (filepath, dirpath):
+    if not os.path.isdir(dirpath):
         filezip = zipfile.ZipFile(filepath)
-        filezip.extractall(os.path.dirname(filepath))
+        filezip.extractall(dirpath)
+        filezip.close()
     #endif
 #enddef
 
 
 if __name__ == "__main__":
     datasetPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Dataset")
+    downloadPath = os.path.join(datasetPath, "nodelink.zip")
+    extractPath = os.path.join(datasetPath, "nodelink")
+    resultPath = os.path.join(datasetPath, "nodeData.csv")
 
     # Download Korea standard node link data
     nodeDataURL = "http://nodelink.its.go.kr/Common/download.aspx?mapPath=/FileData/Pds&fileName=[2019-09-20]%20%EC%A0%84%EA%B5%AD%ED%91%9C%EC%A4%80%EB%85%B8%EB%93%9C%EB%A7%81%ED%81%AC.zip"
-    download(nodeDataURL, os.path.join(datasetPath, "nodelink.zip"))
+    download(nodeDataURL, downloadPath)
+    extract(downloadPath, extractPath)
 
     # Read data
-    shp_path_node = os.path.join(datasetPath, "MOCT_NODE.shp")
+    shp_path_node = os.path.join(extractPath, "MOCT_NODE.shp")
     sf_node = shapefile.Reader(shp_path_node, encoding="cp949")
-    #shp_path_link = os.path.join(datasetPath, "MOCT_LINK.shp")
+    #shp_path_link = os.path.join(extractPath, "MOCT_LINK.shp")
     #sf_link = shapefile.Reader(shp_path_link, encoding="cp949")
 
     ids = []
@@ -55,5 +67,5 @@ if __name__ == "__main__":
     #endfor
 
     df_node = pd.DataFrame({'id': ids, 'name': names, 'latitude': latitude, 'longitude': longitude})
-    df_node.to_csv(os.path.join(datasetPath, "nodeData.csv"), encoding="cp949", index=False)
+    df_node.to_csv(resultPath, encoding="cp949", index=False)
 #endif
