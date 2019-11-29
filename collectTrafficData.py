5f4291c8ede918137269f29dc03485b02ddccfe1
yunik1004 <inkyuhak@gmail.com>
2019-11-29 18:37:29 +0900

    Implement traffic data collector

--- collectTrafficData.py
+++ collectTrafficData.py
@@ -1,36 +1,59 @@
+import os
 import xml.etree.ElementTree as ET
-import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
 import requests
+import yaml
 
 
 if __name__ == "__main__":
-    nodes = pd.read_csv('Dataset/nodeData.csv', encoding='cp949')
+    currentDir = os.path.dirname(os.path.abspath(__file__))
+    datasetPath = os.path.join(currentDir, "Dataset")
+    if not os.path.exists(datasetPath):
+        os.makedirs(datasetPath)
+    #endif
+    settingsPath = os.path.join(currentDir, "config", "settings.yaml")
+    resultPath = os.path.join(datasetPath, "nodeLink.csv")
 
-    graph = nx.DiGraph()
+    with open(settingsPath) as f:
+        conf = yaml.safe_load(f)
+    #endwith
+    authkey = conf['authKey']
 
     # Query openapi road network traffic data
-    apiURL = "http://openapi.its.go.kr/dev/TrafficInfo.xml;jsessionid=533FDD030B9595A85D82E02634DEB80F" # Should be changed
+    apiURL = "http://openapi.its.go.kr/api/NTrafficInfo?&zoom=16&key=" + str(authkey)
     response = requests.get(apiURL)
-    print("Status code : ", response.status_code)
 
     xmlData = response.text
     root = ET.fromstring(xmlData)
 
-    for data in root.findall('data'):
-        startnodeid = int(data.find('startnodeid').text)
-        endnodeid = int(data.find('endnodeid').text)
-        avgspeed = float(data.find('avgspeed').text)
-        traveltime = int(data.find('traveltime').text)
+    sources = []
+    targets = []
+    names = []
+    avgspeeds = []
+    traveltimes = []
+    dates = []
 
-        graph.add_edge(startnodeid, endnodeid, avgspeed=avgspeed, traveltime=traveltime)
+    for data in root.findall('data'):
+        try:
+            startnodeid = int(data.find('startnodeid').text)
+            endnodeid = int(data.find('endnodeid').text)
+            name = data.find('roadnametext').text
+            avgspeed = int(data.find('avgspeed').text)
+            traveltime = int(data.find('traveltime').text)
+            gendate = data.find('generatedate').text
+        except TypeError:
+            pass
+        else:
+            sources.append(startnodeid)
+            targets.append(endnodeid)
+            names.append(name)
+            avgspeeds.append(avgspeed)
+            traveltimes.append(traveltime)
+            dates.append(gendate)
+        #endtry
     #endfor
 
-    fig = plt.figure()
-    pos = nx.spring_layout(graph)
-    nx.draw_networkx_nodes(graph, pos, node_size=30)
-    nx.draw_networkx_edges(graph, pos)
-
-    plt.show()
+    df_link = pd.DataFrame({'source': sources, 'target': targets, 'name': names, 'avgspeed': avgspeeds, 'traveltime': traveltimes, 'generatedate': dates})
+    df_link.to_csv(resultPath, encoding="cp949", index=False)
 #endif
